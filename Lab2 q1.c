ab7c4249c1f3383a6539be935ba87ac39fa0a295
Chirantan Shalya <chirantan.shalya03@gmail.com>
2024-08-19 19:43:24 +0530

    Create Lab2 q1.c

---
+++ Lab2 q1.c
@@ -0,0 +1,19 @@
+//Check if the number is even
+#include <stdio.h>
+
+int main() {
+    int number;
+    
+    // Read the number from user
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    
+    // Check if the number is even or odd
+    if (number % 2 == 0) {
+        printf("%d is even.\n", number);
+    } else {
+        printf("%d is odd.\n", number);
+    }
+    
+    return 0;
+}
